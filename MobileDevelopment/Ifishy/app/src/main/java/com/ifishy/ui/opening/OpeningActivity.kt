099ea41b7a99a0935a66747d2b550006f56c1ce2
bayustwn <khususpby@gmail.com>
2024-11-29 06:25:31 +0700

    feat: adding signUp page

--- MobileDevelopment/Ifishy/app/src/main/java/com/ifishy/ui/opening/OpeningActivity.kt
+++ MobileDevelopment/Ifishy/app/src/main/java/com/ifishy/ui/opening/OpeningActivity.kt
@@ -1,21 +1,50 @@
 package com.ifishy.ui.opening
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.ifishy.R
+import com.ifishy.databinding.ActivityOpeningBinding
+import com.ifishy.ui.auth.SignUpActivity
 
-class OpeningActivity : AppCompatActivity() {
+class OpeningActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding:ActivityOpeningBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_opening)
+        binding = ActivityOpeningBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.getStartedButton.setOnClickListener(this)
+        binding.loginButton.setOnClickListener(this)
+
+    }
+
+    private fun goTo(to: Class<*>){
+        val intent = Intent(this,to)
+        startActivity(intent)
+        finish()
+    }
+
+    override fun onClick(v: View?) {
+        when(v){
+            binding.getStartedButton->{
+                goTo(SignUpActivity::class.java)
+            }
+            binding.loginButton->{
+
+            }
         }
     }
 }
