035df200b90a6eefdb8a6e4b53395c1c4d19e16b
Sandy <aryadikawidodo0@gmail.com>
2024-11-29 10:14:48 +0700

    Error code

--- CloudComputing/middleware/authMiddleware.js
+++ CloudComputing/middleware/authMiddleware.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const secretKey = process.env.JWT_SECRET || 'eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTczMjQ2ODA5NSwiaWF0IjoxNzMyNDY4MDk1fQ.6TTulDMCqgp9hR9YTXY5crLIxKG1W_CSsf3e1DMhm5Q'; // Pastikan JWT_SECRET ada di file .env
+const secretKey = process.env.JWT_SECRET; // Pastikan JWT_SECRET ada di file .env
 
 // Middleware untuk verifikasi token JWT
 const authenticateToken = (req, res, next) => {
