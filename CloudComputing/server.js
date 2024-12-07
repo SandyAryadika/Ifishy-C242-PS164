933a5a68f996ebb9f4613067809c71c2832d55e6
Sandy <aryadikawidodo0@gmail.com>
2024-12-07 15:18:05 +0700

    fixxing features

--- CloudComputing/server.js
+++ CloudComputing/server.js
@@ -1,4 +1,5 @@
-require('dotenv').config();
+const dotenv = require('dotenv');
+dotenv.config();
 const cors = require('cors');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -11,10 +12,15 @@ app.use(bodyParser.json()); // Untuk parsing JSON
 app.use(express.json()); // Untuk menangani JSON
 app.use(express.urlencoded({ extended: true })); // Untuk x-www-form-urlencoded
 
+app.get('/', (req, res) => {
+  console.log('Root route accessed');
+  res.send('Welcome to the API!');
+});
+
 // Daftarkan route
 app.use('/api/auth', authRoutes);
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
