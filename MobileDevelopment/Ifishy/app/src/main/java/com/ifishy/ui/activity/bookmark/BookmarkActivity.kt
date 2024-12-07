7d78250ffb37c61cab8a70306e1a6b2147b6396a
Yow04 <rajawali.prasetya@gmail.com>
2024-12-07 13:08:20 +0700

    feat: add little BookmarkActivity.kt

--- MobileDevelopment/Ifishy/app/src/main/java/com/ifishy/ui/activity/bookmark/BookmarkActivity.kt
+++ MobileDevelopment/Ifishy/app/src/main/java/com/ifishy/ui/activity/bookmark/BookmarkActivity.kt
@@ -5,8 +5,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.tabs.TabLayoutMediator
 import com.ifishy.R
 import com.ifishy.databinding.ActivityBookmarkBinding
+import com.ifishy.ui.adapter.boomark.BookmarkAdapterPage
 
 class BookmarkActivity : AppCompatActivity() {
 
@@ -27,5 +29,16 @@ class BookmarkActivity : AppCompatActivity() {
         binding.back.setOnClickListener {
             finish()
         }
+
+        val adapter = BookmarkAdapterPage(this)
+        binding.viewPager.adapter = adapter
+
+        TabLayoutMediator(binding.selectorbook, binding.viewPager) { tab, position ->
+            tab.text = when (position) {
+                0 -> "Article"
+                1 -> "Post"
+                else -> throw IllegalStateException("Unexpected tab position: $position")
+            }
+        }.attach()
     }
 }
---
+++ MobileDevelopment/Ifishy/app/src/main/java/com/ifishy/ui/adapter/boomark/BookmarkAdapterPage.kt
@@ -0,0 +1,20 @@
+package com.ifishy.ui.adapter.boomark
+
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.ifishy.ui.activity.bookmark.BookmarkActivity
+import com.ifishy.ui.activity.bookmark.fragmentBookmark.BookmarkArticlesFragment
+import com.ifishy.ui.activity.bookmark.fragmentBookmark.BookmarkPostFragment
+
+class BookmarkAdapterPage(activity: BookmarkActivity) : FragmentStateAdapter(activity) {
+
+    override fun getItemCount(): Int = 2
+
+    override fun createFragment(position: Int): Fragment {
+        return when (position){
+            0 -> BookmarkArticlesFragment()
+            1 -> BookmarkPostFragment()
+            else -> throw IllegalStateException("Unexpected position: $position")
+        }
+    }
+}
